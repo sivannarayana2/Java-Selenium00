48731685ee78a93042b0eba2e8d84ac46ce0065b
unknown <mutukurisivannarayana@gmail.com>
2024-07-01 20:35:25 +0530

    CoreJavaBrushUp.java

---
+++ CoreJavaBrushUp.java
@@ -0,0 +1,39 @@
+package Testing;
+
+public class CoreJavaBrushUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n = 29;
+		String name = "siva";
+		char Age='m';
+		double marks=58.3;
+		boolean test=true;
+		System.out.println("the value of n is"+n);
+		System.out.println("the value of name is"+name);
+		System.out.println("the value of characte  is"+Age);
+		System.out.println("the value of marks is"+marks);
+		System.out.println("the value of test is"+test);
+		
+		//arrays
+		
+		int[] a=new int [5];
+		a[0]=2;
+		a[1]=3;
+		a[2]=4;
+		a[3]=5;
+		a[4]=6;
+		
+		
+		int a2[]= {1,2,3,4,5};
+		
+		for(int i=0;i<=a2.length;i++)
+		{
+			System.out.println(a2[i]);
+		}
+				
+
+	}
+
+}
